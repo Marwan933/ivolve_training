a6910912c3f5d404719b7effe046fe3428063579
Marwan Hesham <104536014+Marwan933@users.noreply.github.com>
2025-01-08 17:12:07 +0200

    Delete jenkins/LAB-24 directory

--- jenkins/LAB-24/my-shared-library /vars/deployToK8s.groovy
+++
@@ -1,6 +0,0 @@
-// deployToK8s.groovy
-def call(String kubeConfig, String deploymentYaml, String kubeNamespace) {
-    sh """
-        kubectl --kubeconfig=$kubeConfig apply -f $deploymentYaml -n $kubeNamespace
-    """
-}
